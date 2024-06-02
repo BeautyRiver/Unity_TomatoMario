d7bcd8964d6e0dd7c61dc9c89a2d48edea611761
MinKang Kim <ds5thg@naver.com>
2024-06-03 01:29:19 +0900

    [Make] Detect 기능 추가 완료 Up,Down으로 쉽게 변경가능

---
+++ Assets/Scripts/Cheat & Box Script/CheatScript/ShotEnemyTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Android.Types;
+using UnityEngine;
+
+public class ShotEnemyTrigger : DetectOption
+{
+    private void Awake() 
+    {
+        adaptPos.x = transform.position.x;
+    }
+    protected override void Update()
+    {
+        base.Update();
+        // 위가 감지되면
+        if (isDetectUp)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            isDetectUp = false; // 감지 초기화
+        }
+        // 아래가 감지되면
+        else if (isDetectDown)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            isDetectDown = false; // 감지 초기화
+        }
+    }
+}
