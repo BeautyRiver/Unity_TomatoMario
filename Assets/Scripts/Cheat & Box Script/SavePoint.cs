bf3345ef980f9bbc1bdbed6150833b17a604ca32
BeautyRiver <ds5thg@naver.com>
2024-09-04 11:18:25 +0900

    [Make] 몹 생성 트리거 생성+세이브 포인트 재생성

---
+++ Assets/Scripts/Cheat & Box Script/SavePoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour
+{
+    public Sprite changeSprite;
+    bool isSaved = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player") || isSaved == true)
+            return;
+        
+        PlayerPrefs.SetFloat("SavePointX", transform.position.x);
+        PlayerPrefs.SetFloat("SavePointY", transform.position.y);
+        PlayerPrefs.SetFloat("SavePointZ", transform.position.z);
+
+        gameObject.GetComponentInParent<SpriteRenderer>().sprite = changeSprite;
+        isSaved = true;
+    }
+}
---
+++ Assets/Scripts/Enemys/EnemyTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    public bool isOneOff = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isOneOff || !other.CompareTag("Player"))
+            return;
+
+        // 자식 개수 확인
+        int childCount = gameObject.transform.childCount;
+
+        if (childCount > 0)
+        {
+            // 모든 자식들을 순회
+            for (int i = 0; i < childCount; i++)
+            {
+                // i번째 자식 객체 가져오기
+                GameObject child = gameObject.transform.GetChild(i).gameObject;
+
+                // 자식 객체의 컴포넌트 활성화
+                child.SetActive(true);
+            }
+        }
+        
+        isOneOff = true;
+    }
+}
